81a6a75c51935643c7d6e5caa09334b6e5c1007b
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-06-17 20:57:57 +0530

    feat: add home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,42 @@
+import { BookCard } from "../components/bookcard/BookCard";
+import { useBook } from "../components/context/BookContext";
+import "./Pages.css";
+
+export const Home = () => {
+  const { currReading, alreadyRead, wantToRead } = useBook();
+  return (
+    <>
+      <h1>Currently Reading</h1>
+      <hr />
+      <div className="book-list">
+        {currReading.map((book) => (
+          <div key={book.id}>
+            <BookCard bookObj={book} />
+          </div>
+        ))}
+      </div>
+
+      <h1>Want to Read</h1>
+      <hr />
+
+      <div className="book-list">
+        {wantToRead.map((book) => (
+          <div key={book.id}>
+            <BookCard bookObj={book} />
+          </div>
+        ))}
+      </div>
+
+      <h1>Already Read</h1>
+      <hr />
+
+      <div className="book-list">
+        {alreadyRead.map((book) => (
+          <div key={book.id}>
+            <BookCard bookObj={book} />
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
