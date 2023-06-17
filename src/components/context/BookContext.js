bf2e0631e427fe877425fcaf2040cdacc0a3e29c
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-06-17 21:29:36 +0530

    feat: update category change handler

--- src/components/context/BookContext.js
+++ src/components/context/BookContext.js
@@ -1,17 +1,35 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
 import { books } from "../../data/data";
 
 const BookContext = createContext();
 
 export const BookProvider = ({ children }) => {
-  const currReading = books.filter(
+  const [bookData, setBookData] = useState(books);
+
+  const changeCategoryHandler = (bookId, bookCategory) => {
+    let updatedData = bookData;
+    if (bookCategory === "") {
+      updatedData = bookData.filter((book) => book.id !== bookId);
+    } else {
+      updatedData = bookData.map((book) =>
+        book.id === bookId ? { ...book, category: bookCategory } : book
+      );
+    }
+    setBookData(updatedData)
+  };
+
+  const currReading = bookData.filter(
     (book) => book.category === "Currently Reading"
   );
-  const alreadyRead = books.filter((book) => book.category === "Read");
-  const wantToRead = books.filter((book) => book.category === "Want to Read");
+  const alreadyRead = bookData.filter((book) => book.category === "Read");
+  const wantToRead = bookData.filter(
+    (book) => book.category === "Want to Read"
+  );
 
   return (
-    <BookContext.Provider value={{ currReading, alreadyRead, wantToRead }}>
+    <BookContext.Provider
+      value={{ currReading, alreadyRead, wantToRead, changeCategoryHandler }}
+    >
       {children}
     </BookContext.Provider>
   );
