edf47a1eb20a2619ef95f0e9d1995a35dccca633
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-06-17 20:57:40 +0530

    docs: created data

---
+++ src/data/data.js
@@ -0,0 +1,92 @@
+export const books = [
+  {
+    id: 1,
+    book: "Harry Potter",
+    author: "J.K. Rowling",
+    genre: "Fantasy",
+    category: "Read",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUrifH1GcTas5LWMATm_nhMtXhE0vHnSs4mQ&usqp=CAU",
+  },
+  {
+    id: 2,
+    book: "To Kill a Mockingbird",
+    author: "Harper Lee",
+    genre: "Classic",
+    category: "Read",
+    image:
+      "https://media.glamour.com/photos/56e1f3c4bebf143c52613c0b/master/w_1600%2Cc_limit/entertainment-2016-02-next-main.jpg",
+  },
+  {
+    id: 3,
+    book: "The Great Gatsby",
+    author: "F. Scott Fitzgerald",
+    genre: "Classic",
+    category: "Read",
+    image:
+      "https://m.media-amazon.com/images/I/61z0MrB6qOS._AC_UF1000,1000_QL80_.jpg",
+  },
+  {
+    id: 4,
+    book: "Pride and Prejudice",
+    author: "Jane Austen",
+    genre: "Classic",
+    category: "Read",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTvddXsG9RB4a4Kz6vHhQ4tKFeIT5lHbGHZw&usqp=CAU",
+  },
+  {
+    id: 5,
+    book: "1984",
+    author: "George Orwell",
+    genre: "Dystopian",
+    category: "Want to Read",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhr7_g3SUPAiD3YmXDfBo7Aq1_pd0lQENA7Q&usqp=CAU",
+  },
+  {
+    id: 6,
+    book: "The Catcher in the Rye",
+    author: "J.D. Salinger",
+    genre: "Coming-of-age",
+    category: "Currently Reading",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCk-8pEGo6RyVaE9S2uBLhovRes_sxuPla9Q&usqp=CAU",
+  },
+  {
+    id: 7,
+    book: "To Kill a Kingdom",
+    author: "Alexandra Christo",
+    genre: "Fantasy",
+    category: "Currently Reading",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrK_0KfVeLrS5fKpcNqxumvd3zC4OupdMl9w&usqp=CAU",
+  },
+  {
+    id: 8,
+    book: "The Hobbit",
+    author: "J.R.R. Tolkien",
+    genre: "Fantasy",
+    category: "Currently Reading",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgH8JU-mwWfmb8Eg1tIAxnH3sPF5f5UNAuCw&usqp=CAU",
+  },
+  {
+    id: 9,
+    book: "The Alchemist",
+    author: "Paulo Coelho",
+    genre: "Classic",
+    category: "Want to Read",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTApE7UHwNJgUlqlMfo3pAJZy5KkbyrJqTPw&usqp=CAU",
+  },
+  {
+    id: 10,
+    book: "The Lord of the Rings",
+    author: "J.R.R. Tolkien",
+    genre: "Fantasy",
+    category: "Want to Read",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiXQMswLV5X2xue4ZaeK2IW0oO2OpImglLXA&usqp=CAU",
+  },
+];
