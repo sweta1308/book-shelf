5e812ae89c7d5adcf8dbebb12787dbded2a47ac5
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-06-17 20:58:22 +0530

    style: add style to search page

---
+++ src/pages/Search.jsx
@@ -0,0 +1,18 @@
+import { BookCard } from "../components/bookcard/BookCard";
+import { useFilter } from "../components/context/SearchFilterContext";
+import "./Pages.css";
+
+export const Search = () => {
+  const { filteredBooks } = useFilter();
+  return (
+    <>
+      <div className="book-list">
+      {filteredBooks.length > 0 ? filteredBooks.map((book) => (
+          <div key={book.id}>
+            <BookCard bookObj={book} />
+          </div>
+        )) : <h1>Enter Book or Author Name...</h1>}
+      </div>
+    </>
+  );
+};
